0e97051ad47c96ea3931c5055bfd84b7a400743d
nirvanadam <nirvan.pramudhyta@gmail.com>
2023-03-02 17:30:54 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+toggle.addEventListener('click', function(){
+    menu.classList.toggle('slide');
+});
